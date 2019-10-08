5dd2c196e5b61f1b27b66cfe5f2db4ad1621ca5e
aamir <wasi3aamir@gmail.com>
2019-10-08 23:54:58 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AAMIR.Assignment6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
